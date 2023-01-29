54c4b2171bcccc7afe557fa0acb8597a990cb4e2
Anatoly Fedyanin <anfemail@gmail.com>
2023-01-29 22:43:35 +0300

    next step

--- python/currency-exchange/exchange.py
+++ python/currency-exchange/exchange.py
@@ -1,3 +1,5 @@
+"""Functions for implementing currency exchange."""
+
 def exchange_money(budget, exchange_rate):
     """
 
@@ -5,8 +7,7 @@ def exchange_money(budget, exchange_rate):
     :param exchange_rate: float - unit value of the foreign currency.
     :return: float - exchanged value of the foreign currency you can receive.
     """
-
-    pass
+    return budget / exchange_rate
 
 
 def get_change(budget, exchanging_value):
@@ -17,7 +18,7 @@ def get_change(budget, exchanging_value):
     :return: float - amount left of your starting currency after exchanging.
     """
 
-    pass
+    return budget - exchanging_value
 
 
 def get_value_of_bills(denomination, number_of_bills):
@@ -28,7 +29,7 @@ def get_value_of_bills(denomination, number_of_bills):
     :return: int - total value of bills you now have.
     """
 
-    pass
+    return denomination * number_of_bills
 
 
 def get_number_of_bills(budget, denomination):
@@ -39,7 +40,7 @@ def get_number_of_bills(budget, denomination):
     :return: int - number of bills after exchanging all your money.
     """
 
-    pass
+    return budget // denomination
 
 
 def get_leftover_of_bills(budget, denomination):
@@ -50,7 +51,7 @@ def get_leftover_of_bills(budget, denomination):
     :return: float - the leftover amount that cannot be exchanged given the current denomination.
     """
 
-    pass
+    return budget % denomination
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
@@ -62,5 +63,9 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     :param denomination: int - the value of a single bill.
     :return: int - maximum value you can get.
     """
+    actual = exchange_rate * (1 + spread / 100)
+    exchanged = exchange_money(budget, actual)
+    number_of_bills = get_number_of_bills(exchanged, denomination)
+    value = get_value_of_bills(denomination, number_of_bills)
 
-    pass
+    return int(value)
